fd9275ea0e892671c56d4df74cbe0ea65bd5855e
Enesozkan20 <enesozkanzkn@gmail.com>
2024-12-31 16:43:04 +0100

    first commit

--- getMovies.js
+++
@@ -1,9 +0,0 @@
-const { showMovies } = require("./app");
-
-export async function getMovies(url) {
-    const res = await fetch(url);
-    const data = await res.json();
-
-    console.log(data.results);
-    showMovies(data.results);
-}
